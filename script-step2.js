e941b7d432cd02f6ddfbcd9d9d0cb82e1f49ecd4
Niger <nigerabed@gmail.com>
2023-10-28 22:28:51 +0200

    step3: using function

--- script-step2.js
+++ script-step2.js
@@ -6,19 +6,14 @@ todoList.push("Contact for dantist insurance");
 todoList.push("Kids vaccine");
 todoList.push("Buy Nigers winter jakket ;) ");
 
+//connect with HTML ul element
+let listBox = document.getElementById("listBox");
+
 // using loop
 for (let i = 0; i < todoList.length; i++) {
-  addTasktoListBox(todoList[i]);
-}
-
-//using function
-function addTasktoListBox(todo) {
-  //connect with HTML ul element
-  let listBox = document.getElementById("listBox");
-
   // now create a new li element
   let taskLi = document.createElement("li");
-  taskLi.innerText = todo;
+  taskLi.innerText = todoList[i];
 
   // add the li element as a child to the ul element
   listBox.appendChild(taskLi);
