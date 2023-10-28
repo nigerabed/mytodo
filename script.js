5e541f735be3a14ef97d170c87853cd655002561
Niger <nigerabed@gmail.com>
2023-10-28 21:50:06 +0200

    added basic html and crated an todolist array in js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("my todo");
+// setup
+let todoList = [];
+
+todoList.push("Pay El Bill");
+todoList.push("Contact for dantist insurance");
+todoList.push("Kids vaccine");
+
+console.log(todoList);
